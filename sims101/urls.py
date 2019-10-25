9c8da671b5311289dd2a7d35805d0b11385ecd56
KYD <kydkang@gmail.com>
2019-10-25 16:58:44 -0500

    Merge createview into ListView

--- sims101/urls.py
+++ sims101/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'sims101'
 
 urlpatterns = [ 
-    path('', views.IndexListView.as_view(), name='index_list'), 
+    path('', views.IndexListView, name='index_list'), 
     path('data/<int:pk>/', views.IndexDetailView.as_view(), name='index_detail'), 
-    path('data/create/', views.IndexCreateView.as_view(), name='index_create'), 
+    # path('data/create/', views.IndexCreateView.as_view(), name='index_create'), 
     path('data/<int:pk>/update', views.IndexUpdateView.as_view(), name='index_update'),
     path('data/<int:pk>/delete', views.IndexDeleteView.as_view(), name='index_delete'), 
     path('ajax/change_session/', views.ajax_change_session, name='ajax_change_session'),  
--- sims101/views.py
+++ sims101/views.py
@@ -1,3 +1,4 @@
+from django.views import View
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy 
@@ -8,48 +9,79 @@ from commons.models import Description
 from django.http import HttpResponseRedirect
 
 
-class IndexListView(PermissionRequiredMixin, ListView):
-    permission_required = ('sims101.index-contributor') 
-    model = Index101                      ###  Or,   queryset = Post.objects.all()
-    template_name = 'sims101/index_list.html'   ### default context name is 'object_list'. To change it, enter context_object_name = 'posts'
-    # paginate_by = 3       ## 3 objects per page 
+# class IndexListView(PermissionRequiredMixin, ListView):
+#     permission_required = ('sims101.index-contributor') 
+#     model = Index101                      ###  Or,   queryset = Post.objects.all()
+#     template_name = 'sims101/index_list.html'   ### default context name is 'object_list'. To change it, enter context_object_name = 'posts'
+#     # paginate_by = 3       ## 3 objects per page 
 
-    def get_context_data(self, **kwargs):   ### get the first object to be used in the index_list.html 
-        context = super(IndexListView, self).get_context_data(**kwargs) 
-        context['first'] = Index101.objects.first()  
-        context['description'] = Description.objects.get(sequence=Index101.SEQUENCE)
-        if not ('form' in context):
-            context['form'] = IndexForm()
+#     def get_context_data(self, **kwargs):   ### get the first object to be used in the index_list.html 
+#         context = super(IndexListView, self).get_context_data(**kwargs) 
+#         context['first'] = Index101.objects.first()  
+#         context['description'] = Description.objects.get(sequence=Index101.SEQUENCE)
+#         if not ('form' in context):
+#             context['form'] = IndexForm()
+#         return context
+ 
+from django.core.paginator import Paginator, EmptyPage,  PageNotAnInteger
+def IndexListView(request):
+    object_list = Index101.objects.all() 
+    first = Index101.objects.first()  
+    description= Description.objects.get(sequence=Index101.SEQUENCE)
 
-        return context
+    paginator = Paginator(object_list, 333)
+    page = request.GET.get('page')
+    try:
+        object_list = paginator.page(page)
+    except PageNotAnInteger:
+        object_list =  paginator.page(1)
+    except EmptyPage: 
+        object_list = paginator.page(paginator.num_pages) 
+
+    if request.method == 'POST':
+        form = IndexForm(request.POST)
+        context = {'form':form, 'object_list':object_list, 'first':first, 'description':description}
+        if form.is_valid():              
+            book = form.save()
+            book.save()
+            request.session['created'] = "true"
+            # request.session.modified = True
+            return render(request, 'sims101/index_list.html', context)
+    else:
+        form = IndexForm()
+        context = {'form':form, 'object_list':object_list, 'first':first, 'description':description}
+    return render(request, 'sims101/index_list.html', context)
+
+
+
 
 class IndexDetailView(PermissionRequiredMixin, DetailView):
     permission_required = ('sims101.index-contributor') 
     model = Index101
     template_name = 'sims101/index_detail.html' 
 
-class IndexCreateView(PermissionRequiredMixin, CreateView):
-    permission_required = ('sims101.index-contributor') 
-    model = Index101
-    form_class = IndexForm 
-    template_name = 'sims101/index_create.html'
-    login_url = 'login'
-    success_url = reverse_lazy('sims101:index_list')  
-    ### CreateView, UpdateView에 success_url을 제공하지 않는 경우, 해당 model instance의 get_absolute_url 주소로 이동이 가능한지 체크한다 by Django ]]
+# class IndexCreateView(PermissionRequiredMixin, CreateView):
+#     permission_required = ('sims101.index-contributor') 
+#     model = Index101
+#     form_class = IndexForm 
+#     template_name = 'sims101/index_create.html'
+#     login_url = 'login'
+#     success_url = reverse_lazy('sims101:index_list')  
+#     ### CreateView, UpdateView에 success_url을 제공하지 않는 경우, 해당 model instance의 get_absolute_url 주소로 이동이 가능한지 체크한다 by Django ]]
 
-    def form_invalid(self, form):  
-        first = Index101.objects.first()  
-        description = Description.objects.get(sequence=Index101.SEQUENCE)  
-        object_list = Index101.objects.all()  
-        context = {'first':first, 'description':description, 'form':form, 'object_list':object_list} 
-        return render(self.request, 'sims101/index_list.html', context)
+#     def form_invalid(self, form):  
+#         first = Index101.objects.first()  
+#         description = Description.objects.get(sequence=Index101.SEQUENCE)  
+#         object_list = Index101.objects.all()  
+#         context = {'first':first, 'description':description, 'form':form, 'object_list':object_list} 
+#         return render(self.request, 'sims101/index_list.html', context)
 
-        # return HttpResponseRedirect('/101/')
+#         # return HttpResponseRedirect('/101/')
 
-    def setup(self, request, *args, **kwargs): 
-        super().setup(request, *args, **kwargs)
-        request.session['created'] = "true"
-        request.session.modified = True
+#     def setup(self, request, *args, **kwargs): 
+#         super().setup(request, *args, **kwargs)
+#         request.session['created'] = "true"
+#         request.session.modified = True
 # 
 #  You can populate with some  initialization data for the form. 
     # def get_initial(self, *args, **kwargs):
@@ -63,10 +95,11 @@ def ajax_change_session(request):
     request.session['created'] = ""
     return render(request, 'sims101/index_delete.html') 
 
+from decimal import Decimal
 def ajax_calculate(request):
     first_data = request.GET.get('first_data')
     second_data = request.GET.get('second_data')
-    final_value = int(first_data) * int(second_data)   ###  same as defined in model.py 
+    final_value = int(first_data) * Decimal(second_data)   ###  same as defined in model.py 
     return render(request, 'sims101/final_value.html', {'final_value':final_value})
 
 class IndexUpdateView(PermissionRequiredMixin, UpdateView):
